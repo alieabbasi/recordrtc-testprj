d4f67d91c15a9e71b9dcb60b25c010e96f304863
Ali Abbasi <aliabac7887@gmail.com>
2022-08-03 13:20:44 +0430

    change cam while recording test 1

--- components/Recorder.tsx
+++ components/Recorder.tsx
@@ -1,6 +1,7 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { FC } from "react";
 import { useEffect, useRef, useState } from "react";
-import RecordRTC from "recordrtc";
+import RecordRTC, { MediaStreamRecorder, MultiStreamRecorder } from "recordrtc";
 import { Icon, IconifyIcon } from "@iconify/react";
 import record12Regular from "@iconify/icons-fluent/record-12-regular";
 import arrowCounterclockwise12Regular from "@iconify/icons-fluent/arrow-counterclockwise-12-regular";
@@ -20,7 +21,7 @@ interface RecorderProps {}
 
 const Recorder: FC<RecorderProps> = () => {
   const [state, setState] = useState<States>(States.IDLE);
-  const [recorder, setRecorder] = useState<RecordRTC | null>(null);
+  const [recorder, setRecorder] = useState<MultiStreamRecorder | null>(null);
   const [stream, setStream] = useState<MediaStream | null>(null);
   const [currentCam, setCurrentCam] = useState<"user" | "environment">("environment");
   const [sizes, setSizes] = useState<{ width: number | undefined; height: number | undefined }>();
@@ -53,7 +54,7 @@ const Recorder: FC<RecorderProps> = () => {
 
   const startDevice = async () => {
     await getUserMedia(currentCam);
-    // getDevices();
+    getDevices();
   };
 
   const getMaxSizes = async (facingMode: "user" | "environment") => {
@@ -130,6 +131,10 @@ const Recorder: FC<RecorderProps> = () => {
       if (videoElRef.current) {
         videoElRef.current.srcObject = stream;
         setStream(stream);
+
+        if (recorder) {
+          recorder.resetVideoStreams([stream]);
+        }
       }
     } catch (err) {
       alert("Unable to capture your camera. Please check console logs.");
@@ -145,7 +150,6 @@ const Recorder: FC<RecorderProps> = () => {
     stream.getTracks().forEach((track) => {
       track.stop();
     });
-
     if (currentCam === "user") {
       getUserMedia("environment");
       setCurrentCam("environment");
@@ -174,29 +178,30 @@ const Recorder: FC<RecorderProps> = () => {
 
   const startRecording = () => {
     if (stream) {
-      const recorder = new RecordRTC(stream, { type: "video" });
-      recorder.startRecording();
+      const recorder = new MultiStreamRecorder([stream], { type: "video", recorderType: MultiStreamRecorder });
+      console.log("RECORDER:", recorder);
+      recorder.record();
       setRecorder(recorder);
       setState(States.RECORDING);
     }
   };
 
   const resumeRecording = () => {
-    recorder?.resumeRecording();
+    recorder?.resume();
     setState(States.RECORDING);
   };
 
   const pauseRecording = () => {
-    recorder?.pauseRecording();
+    recorder?.pause();
     setState(States.PAUSED);
   };
 
   const stopRecording = async () => {
     if (videoElRef.current) {
-      recorder?.stopRecording(() => {
+      recorder?.stop((blob) => {
         videoElRef.current!.srcObject = null;
-        videoElRef.current!.src = window.URL.createObjectURL(recorder!.getBlob());
-        recorder?.destroy();
+        videoElRef.current!.src = window.URL.createObjectURL(blob);
+        recorder?.clearRecordedData();
         setRecorder(null);
         setState(States.RECORDED);
       });
@@ -212,7 +217,7 @@ const Recorder: FC<RecorderProps> = () => {
     <div className="w-screen h-screen flex justify-center items-center">
       <div className="h-full max-h-full w-full max-w-full bg-slate-600 relative flex justify-center items-center">
         <div className="w-max object-cover relative">
-          {state === States.IDLE && (
+          {(state !== States.RECORDED) && (
             <div className="flex absolute top-10 right-10 justify-center items-center">
               <button className="z-10 bg-black/50 text-white p-2 rounded-full" onClick={changeDevice}>
                 <Icon icon={cameraSwitch20Filled} className="text-6xl" />
